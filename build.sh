d74f1742d997574c6b3cf57c513f4a744189e9ca
davidg <davidg>
2004-07-05 13:09:52 +0000

    improved script to test for non-CA dirs and write RPMS in new dir

--- build.sh
+++ build.sh
@@ -11,8 +11,17 @@ case $# in
 0 )	echo "Usage: $0 <version>" >&2 ; exit 1 ;;
 esac
 
+[ -d RPMS ] || mkdir RPMS
+[ -d SRPMS ] || mkdir SRPMS
+[ -d tgz ] || mkdir tgz
+
 for ca in "$@"
 do
+	if [ ! -d $ca ]; then
+		echo "$ca is not a directory, skipped" >&2
+		continue
+	fi
+
 	if [ -f $ca/CVS/Tag ]; then
 		version=`sed -e 's/^N//' < $ca/CVS/Tag`
 	else
@@ -22,7 +31,12 @@ do
 	if [ -f $ca/hash ]; then
 		hash=`cat $ca/hash`
 	else
-		hash=`ls -1 $ca/*.0 | sed -e 's/.*\///;s/\.0$//'`
+		hash=`ls -1 $ca/*.0 2>/dev/null | sed -e 's/.*\///;s/\.0$//'`
+	fi
+
+	if [ x"$hash" = x"" ] ; then
+		echo "$ca is not a CA dir" >&2
+		continue
 	fi
 
 	echo "CA $ca: building version $version release $release for hash $hash"
@@ -38,15 +52,16 @@ do
 	cp -p ca_$ca.spec $rpmtop/SPECS/
 
 	rpmbuild -ba $rpmtop/SPECS/ca_$ca.spec
-	cp -p $rpmtop/RPMS/noarch/ca_$ca-$version-$release.noarch.rpm .
-	cp -p $rpmtop/SRPMS/ca_$ca-$version-$release.src.rpm .
-	cp -p $rpmtop/SOURCES/$ca-$version.tar.gz .
 	echo Build RPM and tar for version $version of CA $ca and copied it here.
 
 	)
 
-	ls -l $ca/ca_$ca-$version-$release.noarch.rpm
-	ls -l $ca/ca_$ca-$version-$release.src.rpm
-	ls -l $ca/$ca-$version.tar.gz
+	cp -p $rpmtop/RPMS/noarch/ca_$ca-$version-$release.noarch.rpm ./RPMS
+	cp -p $rpmtop/SRPMS/ca_$ca-$version-$release.src.rpm ./SRPMS
+	cp -p $rpmtop/SOURCES/$ca-$version.tar.gz ./tgz
+
+	ls -l RPMS/ca_$ca-$version-$release.noarch.rpm
+	ls -l SRPMS/ca_$ca-$version-$release.src.rpm
+	ls -l tgz/$ca-$version.tar.gz
 
 done
