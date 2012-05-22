4762a8e09c5411c15fda7a24fd4e5fc5ade6da7b
pmacvsdg <pmacvsdg>
2012-05-22 12:49:26 +0000

    Fix CA URL for DFN root cert as well

--- buildtools/cabuild3.pl
+++ buildtools/cabuild3.pl
@@ -1,6 +1,6 @@
 #! /usr/bin/perl -w
 #
-# @(#)$Id: cabuild3.pl,v 1.23 2012/03/29 08:10:06 pmacvsdg Exp $
+# @(#)$Id: cabuild3.pl,v 1.24 2012/04/23 15:34:03 pmacvsdg Exp $
 #
 # The IGTF CA build script
 #
@@ -1012,7 +1012,7 @@ sub packSingleCA($$$$) {
     foreach my $url ( split(/[; ]+/,$info{"crl_url"}) ) {
       print CRLURL "$url\n";
       # check CRL for consistency if http
-      if ( $url =~ /^http:/ and ($info{"alias"} !~ ".*(Br|LAC).*") ) {
+      if ( $url =~ /^http:/ and ($info{"alias"} !~ ".*(HIAST).*") ) {
         my $response;
         chomp ( $response = `GET \'$url\' | openssl crl -CAfile $srcdir/$basename.0 -inform der -noout 2>&1` );
         chomp ( $response = `GET \'$url\' | openssl crl -CAfile $srcdir/$basename.0 -inform pem -noout 2>&1` ) if ( $response ne "verify OK" );
